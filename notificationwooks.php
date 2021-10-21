fb406a3ef0f97b10956662b062366bef773d5dd4
Adriano Almeida Fonseca <adfonseca@br00000036syt53.ml.com>
2021-10-21 12:53:50 -0300

    test changes notification

--- notificationwooks.php
+++ notificationwooks.php
@@ -1,14 +1,7 @@
 <?php
 
-// ini_set('display_errors',1);
-// ini_set('display_startup_erros',1);
-// error_reporting(E_ALL);
-
 // SDK do Mercado Pago
-require __DIR__ . '/vendor/autoload.php';
-
-//Credenciais do Mercado Pago
-MercadoPago\SDK::setAccessToken("APP_USR-8401694271692686-050614-591d4bcd58a81a7d4809badfed6bee6d-532818857");
+require __DIR__ . '/../config.php';
 
   switch($_GET["type"]) {
         case "payment":
@@ -27,34 +20,5 @@ MercadoPago\SDK::setAccessToken("APP_USR-8401694271692686-050614-591d4bcd58a81a7
               echo "Pagamento cancelado: ".$payment->id;
              };
             break;
-      
-//         case "subscription":
-//             $preapproval = null;
-//             $preapproval = MercadoPago\subscription::find_by_id($_GET["data_id"]);
-      
-//            if ($preapproval->status == "pending"){
-//             echo "Assinatura pendente: ".$preapproval->id;
-//            }; 
-       
-//            if ($preapproval->status == "authorized"){
-//               echo "Assinatura autorizada: ".$preapproval->id;
-//            };
-
-//            if ($preapproval->status == "cancelled"){
-//               echo "Assinatura cancelada: ".$preapproval->id;
-//            };
-//            break;
     }
-
-
-
-
-   
-
-//echo "https://IXexamplesPHP-dx979308.codeanyapp.com/notificationwooks.php?data.id=13852152933&type=payment"
-//echo $_GET
-
-//$pagamento = $_POST["pagamento"];
-//$_GET = "https://loja-dx979308.codeanyapp.com/notificationwooks.php?data.id=$pagamento&type=payment";
-
 ?>
